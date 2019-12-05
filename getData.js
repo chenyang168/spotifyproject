1340c941e017e5c4105326595424bef0ba3aeec9
chenyang168 <57324217+chenyang168@users.noreply.github.com>
2019-12-04 19:49:55 -0500

    Add files via upload

--- getData.js
+++ getData.js
@@ -1,18 +1,12 @@
 var client_id = '99502aa428d041faaa173a5302ad498b';
 var client_secret = 'da0cc8cf6d9a4ee085db63a4a0fe62b6';
-// var getTokenUrl = 'https://accounts.spotify.com/api/token';
 var Base64Encode = btoa(client_id+':'+client_secret);
 var auth = 'Basic ' + Base64Encode;
 var myTrackUrl = "https://api.spotify.com/v1/me/tracks";
 var proxyurl = 'https://cors-anywhere.herokuapp.com/';
+var offset = 0
 
-function getParameterByName(url,name) {
-    var name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(url);
-    return results == null ? "": decodeURIComponent(results[1]);
-}
-// the above function is from https://www.cnblogs.com/season-huang/p/3322561.html
+
 function getMyTracks(token) {
 	var params = {'offset':0, 'limit': 50}
 	$.ajax({
@@ -25,6 +19,9 @@ function getMyTracks(token) {
     	dataType: 'json',
 	}).then(function(data) {
 		console.log(data);
+		var totalnum = data.total
+		var times = Math.ceil(totalnum / 50) - 1
+		console.log(times)
 	});
 }
 
@@ -36,27 +33,5 @@ $(function() {
     return result;
 	}, {});
 	getMyTracks(result.access_token)
-	// var access_token_2 = getParameterByName(url, 'access_token');
-	// var type = getParameterByName(url, 'token_type');
-	// var expires_in = getParameterByName(url, 'expires_in');
-	
-	
-    // var grant_type = 'authorization_code';
-    // var redirect_uri = 'https://yinyinumsi.github.io/649GroupProject/main.html';
-    // var params = {	code: code,
-	// 		        redirect_uri: redirect_uri,
-	// 		        grant_type: grant_type,
-	// 		     };
-    // $.ajax({
-    // 	url: proxyurl + getTokenUrl,
-    // 	headers: {
-    // 		'Authorization': auth,
-    // 	},
-    // 	method: 'POST',
-    // 	data: jQuery.param(params),
-    // 	dataType: 'json',
-    // }).then(function(data) {
-    // 	console.log(data.access_token);
-    // 	getMyTracks(data.access_token);
-    // });  
+
 });
